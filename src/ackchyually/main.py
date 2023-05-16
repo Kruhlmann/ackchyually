6e60d5caddf1444b95330650e450df342373bf93
Andreas Krühlmann <andreas@kruhlmann.dev>
2023-05-16 10:58:10 +0200

    Add openai key argumetn

--- src/ackchyually/main.py
+++ src/ackchyually/main.py
@@ -5,9 +5,12 @@ import click
 @click.command()
 @click.argument("url")
 @click.option("--yt-api-key", "-k", help="YouTube API key")
-def main(url: str, yt_api_key: Optional[str] = None) -> None:
+@click.option("--oa-api-key", "-o", help="OpenAI API key")
+def main(url: str, yt_api_key: Optional[str] = None, oa_api_key: Optional[str] = None) -> None:
     click.echo(f"You entered YouTube URL: {url}")
     if yt_api_key:
+        click.echo(f"You entered YouTube API key: {yt_api_key}")
+    if oa_api_key:
         click.echo(f"You entered YouTube API key: {yt_api_key}")
 
 if __name__ == "__main__":
